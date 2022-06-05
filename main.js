4511faa576b31437057a394d2d4e490a86956f36
kaiyang <gamedevleo@gmail.com>
2022-06-05 23:58:32 +1000

    3d rotate around globe

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const imgEle = document.querySelector('img');
 const img1Ele = document.querySelector('.img1');
 let a = 0;
 setInterval(() => {
-  a += 0.1;
+  a += 0.01;
   console.log(imgEle.style.left);
   imgEle.style.left = `${150 + Math.sin(a) * 150}px`;
   imgEle.style.top = `${250 + Math.cos(a) * 250}px`;
   img1Ele.style.left = `${100 + Math.sin(a * 0.5) * 100}px`;
   img1Ele.style.top = `${200 + Math.cos(a * 0.5) * 200}px`;
-}, 100);
+}, 10);
